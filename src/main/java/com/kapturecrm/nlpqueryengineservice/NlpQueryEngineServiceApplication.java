9c1ed4460a56a92d1ce23740e08cbd8ced46fbf9
Yasvanth <yasvanth.kumar@kapturecrm.com>
2024-04-04 17:02:58 +0530

    First Commit

---
+++ src/main/java/com/kapturecrm/nlpqueryengineservice/NlpQueryEngineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kapturecrm.nlpqueryengineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpQueryEngineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpQueryEngineServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kapturecrm/nlpqueryengineservice/NlpQueryEngineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapturecrm.nlpqueryengineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpQueryEngineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
