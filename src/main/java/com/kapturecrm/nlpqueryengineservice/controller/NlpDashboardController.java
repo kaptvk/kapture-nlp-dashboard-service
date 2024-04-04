51de78cd27f9344fc621c23f8020163d9ade2a93
Balaji_R <balaji.r@kapturecrm.com>
2024-04-04 18:11:10 +0530

    Refactor

---
+++ src/main/java/com/kapturecrm/nlpqueryengineservice/controller/NlpDashboardController.java
@@ -0,0 +1,22 @@
+package com.kapturecrm.nlpqueryengineservice.controller;
+
+import com.kapturecrm.nlpqueryengineservice.dto.NlpDashboardReqDto;
+import com.kapturecrm.nlpqueryengineservice.service.NlpDashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("nlp-dashboard")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class NlpDashboardController {
+    private final NlpDashboardService nlpDashboardService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<?> getDataFromNlp(@RequestBody NlpDashboardReqDto reqDto) {
+        return nlpDashboardService.generateNlpDashboard(reqDto);
+    }
+
+}
--- src/main/java/com/kapturecrm/nlpqueryengineservice/service/NlpDashboardService.java
+++ src/main/java/com/kapturecrm/nlpqueryengineservice/service/NlpDashboardService.java
@@ -1,19 +1,23 @@
 package com.kapturecrm.nlpqueryengineservice.service;
 
 
+import com.kapturecrm.nlpqueryengineservice.dto.NlpDashboardReqDto;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class KapGuruService {
+public class NlpDashboardService {
+
     @Value("${openai.apiKey}")
     private String apiKey;
-    public ResponseEntity<?> getDataFromNlp(String text) {
+
+    public ResponseEntity<?> generateNlpDashboard(NlpDashboardReqDto reqDto) {
         OpenAiChatModel model = OpenAiChatModel.withApiKey(apiKey);
-        String answer = model.generate(text);
+        String answer = model.generate(reqDto.getPrompt());
         System.out.println(answer);
         return ResponseEntity.ok(answer);
     }
+
 }
